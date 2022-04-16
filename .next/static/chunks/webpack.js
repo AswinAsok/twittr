564f38fe4fe9831fb609d45070127a78665c793b
Aswin Asok <aswinasokofficial@gmail.com>
2022-04-16 22:56:22 +0530

    Minor Changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1625842c2c9d89be"; }
+/******/ 		__webpack_require__.h = function() { return "86313031e3bf402c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
