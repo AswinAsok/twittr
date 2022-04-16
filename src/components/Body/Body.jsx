3494ef6b2faf9dd699a3fd8ae5684cb378cc334e
Aswin Asok <aswinasokofficial@gmail.com>
2022-04-16 19:31:24 +0530

    CORS Came

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,7 +1,26 @@
+import axios from "axios";
 import React, { useState } from "react";
 import "./Body.css";
 
 // https://api.twitter.com/2/tweets?ids=1511678199616335872&tweet.fields=public_metrics&expansions=attachments.media_keys&media.fields=public_metrics
+
+const getData = async (id) => {
+  let url = `https://api.twitter.com/2/tweets?ids=${id}&tweet.fields=public_metrics&expansions=attachments.media_keys&media.fields=public_metrics`;
+  try {
+    let res = await axios.get(url, {
+      headers: {
+        Authorization:
+           `Bearer ${process.env.REACT_APP_BEARER_TOKEN}`,
+      },
+    });
+
+    console.log("Kitti Guys");
+    return "success";
+  } catch (error) {
+    console.log(error);
+    return "error";
+  }
+};
 
 const Body = () => {
   const [tweetid, setTweetid] = useState(0);
@@ -11,7 +30,7 @@ const Body = () => {
       <input
         onChange={(event) => {
           setTweetid(event.target.value);
-          console.log(tweetid)
+          console.log(tweetid);
         }}
         type="text"
         name=""
@@ -20,6 +39,7 @@ const Body = () => {
       <button
         onClick={() => {
           console.log(tweetid);
+          getData(tweetid);
         }}
         className="search"
       >
